259c954df91f0ff907200e7c6044138d4fc0cbdd
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-05-04 23:31:20 +0600

    Refactor addCourseToStudent function to include additional student properties

--- typescript-technocrat/module-2/2.6.ts
+++ typescript-technocrat/module-2/2.6.ts
@@ -1,6 +1,33 @@
 {
-    // Constrains
+  // Constrains
 
-    
+  const addCourseToStudent = <
+    T extends {
+      id: number;
+      name: string;
+      email: string;
+    }
+  >(
+    student: T
+  ) => {
+    const course = "Next Level Web Development";
 
+    return {
+      ...student,
+      course,
+    };
+  };
+
+  const student1 = addCourseToStudent({
+    name: "X",
+    email: "X@gmail.com",
+    id: 25,
+  });
+
+  const student2 = addCourseToStudent({
+    name: "X",
+    email: "X@gmail.com",
+    id: 16,
+    hasWatch: "Apple Watch",
+  });
 }
